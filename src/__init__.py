9f8f4a263a450cfe4f4abf2c8aed046c924d96f1
Tim Hyams <txh816@alumni.bham.ac.uk>
2023-03-02 22:09:04 +0000

    Create wrangling notebook; add config to init
    Begin data wrangling and mapping tests in notebook. Add mapping dict
    to init to enable import across scripts.

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+# Country name to league name mapping
+country_mappings = {'Germany': '1 Bundesliga',
+ 'Netherlands': 'Eredivisie',
+ 'Portugal': 'Liga Nos',
+ 'France': 'Ligue 1',
+ 'United Kingdom': 'Premier League',
+ 'Spain': 'Primera Division',
+ 'Italy': 'Serie A'}
